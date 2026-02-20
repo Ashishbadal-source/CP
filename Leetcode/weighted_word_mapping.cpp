64f8639fed656832de9bb0d49adeb8a21cbfef34
ASHISH BADAL <idh4816@gmail.com>
2026-02-20 22:08:38 +0530

    lc

--- Leetcode/weighted_word_mapping.cpp
+++ Leetcode/weighted_word_mapping.cpp
@@ -35,3 +35,15 @@
 //         return arr;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int scoreOfString(string s) {
+//         int sum=0;
+//         for(int i=0;i<s.size()-1;i++){
+//             sum+= abs(int(s[i]-s[i+1]));
+//         }
+//         return sum;
+//     }
+// };
