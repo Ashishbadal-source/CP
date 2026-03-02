a4d1080b381a4bec64f00046395ffc1ec24a49e9
ASHISH BADAL <idh4816@gmail.com>
2026-03-02 20:15:59 +0530

    lc

--- Leetcode/array_1.cpp
+++ Leetcode/array_1.cpp
@@ -10,3 +10,32 @@
 //         return arr;
 //     }
 // };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> leftRightDifference(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>leftsum;
+//         vector<int>rightsum;
+//         vector<int>ans;
+//         int sum1 =0;
+//         int sum2=0;
+//         for(int i=0;i<n;i++){
+//             leftsum.push_back(sum1);
+//             sum1 += nums[i];
+//         }
+//         for(int i=0;i<n;i++){
+//             rightsum.push_back(sum2);
+//             sum2 += nums[n-i-1];
+//         }
+
+//         for(int i=0;i<n;i++){
+//             ans.push_back(abs(leftsum[i]-rightsum[n-1-i]));
+//         }
+//         return ans;
+//     }
+// };
